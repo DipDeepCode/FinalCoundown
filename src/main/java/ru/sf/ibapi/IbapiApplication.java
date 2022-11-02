2df58459d36ecb78a1149f561dd55f71f5413cec
Sergey Kovalev <dipdeepcode@mail.ru>
2022-11-02 22:55:02 +0300

    Init commit

---
+++ src/main/java/ru/sf/ibapi/IbapiApplication.java
@@ -0,0 +1,13 @@
+package ru.sf.ibapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sf/ibapi/IbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sf.ibapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
