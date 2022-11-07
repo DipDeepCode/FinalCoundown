d0c028d6600a2e5be944fba803bbd248f5d35b85
Sergey Kovalev <dipdeepcode@mail.ru>
2022-11-07 12:31:58 +0300

    Добавлен springdoc-openapi-ui

--- src/main/java/ru/sf/ibapi/controllers/CustomerController.java
+++ src/main/java/ru/sf/ibapi/controllers/CustomerController.java
@@ -1,5 +1,6 @@
 package ru.sf.ibapi.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.sf.ibapi.apiresponses.responses.ApiResponse;
@@ -17,12 +18,14 @@ public class CustomerController {
     private final ApiResponseBuilder apiResponseBuilder;
 
     @GetMapping("/balance/get")
+    @Operation(summary = "Get balance", description = "Получить текущий баланс пользователя")
     public ApiResponse getBalance(@RequestParam Long id) {
         long balance = customerService.getBalance(id);
         return apiResponseBuilder.buildBalanceResponse(balance);
     }
 
     @PutMapping("/balance/put")
+    @Operation(summary = "Put money", description = "Пополнение баланса пользователя на заданную сумму")
     public ApiResponse putMoney(@RequestParam Long id,
                                 @RequestParam Long amount) throws ChangeBalanceException {
         customerService.putMoney(id, amount);
@@ -30,6 +33,7 @@ public class CustomerController {
     }
 
     @PutMapping("/balance/take")
+    @Operation(summary = "Take money", description = "Снятие заданной суммы с баланса пользователя")
     public ApiResponse takeMoney(@RequestParam Long id,
                                  @RequestParam Long amount) throws ChangeBalanceException {
         customerService.takeMoney(id, amount);
